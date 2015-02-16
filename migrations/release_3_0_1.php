a955a2b220343eafb93b183de9aa2d9cfa140ccd
PayBas <contact@paybas.com>
2015-02-16 11:20:13 +0100

    Fix MSSQL migration 3

--- migrations/release_3_0_1.php
+++ migrations/release_3_0_1.php
@@ -32,13 +32,13 @@ class release_3_0_1 extends \phpbb\db\migration\migration
 
 	public function bump_pb_wow_level()
 	{
-		$sql = 'UPDATE ' . PROFILE_FIELDS_TABLE . ' SET field_maxlen = 100 WHERE field_name = "pb_wow_level"';
+		$sql = "UPDATE " . PROFILE_FIELDS_TABLE . " SET field_maxlen = 100 WHERE field_name = 'pb_wow_level'";
 		$this->db->sql_query($sql);
 	}
 
 	public function revert_pb_wow_level()
 	{
-		$sql = 'UPDATE ' . PROFILE_FIELDS_TABLE . ' SET field_maxlen = 90 WHERE field_name = "pb_wow_level"';
+		$sql = "UPDATE " . PROFILE_FIELDS_TABLE . " SET field_maxlen = 90 WHERE field_name = 'pb_wow_level'";
 		$this->db->sql_query($sql);
 	}
 }
